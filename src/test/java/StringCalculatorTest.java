9838a6c9c10d12cd758f7934384513f3cdc7622f
codebull <codebull707@gmail.com>
2021-08-25 23:14:27 +0530

    Added empty test case 🧪

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class StringCalculatorTest {
+    @Test
+    public void empty(){
+        // Start with the 'minimum' possible test case and check if program is working fine...
+    }
+}
